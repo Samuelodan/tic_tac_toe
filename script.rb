1b4da186d90d3219246f778149d228137c3b78ef
Samuel ODaniels <samodan195@gmail.com>
2022-05-10 20:03:14 +0100

    Add winning_symbol method to Board class

--- script.rb
+++ script.rb
@@ -18,4 +18,32 @@ class Board
   #{grid[4]} | #{grid[5]} | #{grid[6]} \n
   #{grid[7]} | #{grid[8]} | #{grid[9]}"
   end
+
+  def winning_symbol
+    x = :x
+    o = :o
+
+    if grids[1] == x && grids[2] == x && grids[3] == x || # 123
+       grids[4] == x && grids[5] == x && grids[6] == x || # 456
+       grids[7] == x && grids[8] == x && grids[9] == x || # 789
+       grids[1] == x && grids[4] == x && grids[7] == x || # 147
+       grids[2] == x && grids[5] == x && grids[8] == x || # 258
+       grids[3] == x && grids[6] == x && grids[9] == x || # 369
+       grids[1] == x && grids[5] == x && grids[9] == x || # 159
+       grids[3] == x && grids[5] == x && grids[7] == x # 357
+
+      x
+
+    elsif grids[1] == o && grids[2] == o && grids[3] == o || # 123
+          grids[4] == o && grids[5] == o && grids[6] == o || # 456
+          grids[7] == o && grids[8] == o && grids[9] == o || # 789
+          grids[1] == o && grids[4] == o && grids[7] == o || # 147
+          grids[2] == o && grids[5] == o && grids[8] == o || # 258
+          grids[3] == o && grids[6] == o && grids[9] == o || # 369
+          grids[1] == o && grids[5] == o && grids[9] == o || # 159
+          grids[3] == o && grids[5] == o && grids[7] == o # 357
+
+      o
+    end
+  end
 end
