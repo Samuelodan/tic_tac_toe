f6f4e000db1f1e5f0624e6091cd7b2e78792e6de
Samuel ODaniels <samodan195@gmail.com>
2022-05-28 22:42:04 +0100

    Move Game class to separate file

---
+++ game.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: false
+
+require './board'
+require './players'
+
+# creates game object that contains other objects
+class Game
+  attr_accessor :board
+  attr_reader :player1, :player2
+
+  def initialize
+    @board = Board.new
+    @player1 = Players.new
+    @player2 = Players.new
+  end
+
+  def assign_sym
+    @player1.symbol = :x
+    @player2.symbol = :o
+    puts "player1 is x\nplayer2 is o"
+  end
+
+  def check_winner
+    case board.winning_symbol
+    when :x
+      puts 'Player1 has won the game'
+      board.moves_left = false
+    when :o
+      puts 'Player2 has won the game'
+      board.moves_left = false
+    else
+      false
+    end
+  end
+end
