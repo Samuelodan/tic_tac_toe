092cf2163d1c6150d34ae60438cc0f5658e7f962
Samuel ODaniels <samodan195@gmail.com>
2022-05-28 22:41:17 +0100

    Move Board class to separate file

---
+++ board.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: false
+
+# creates tic tac toe grids
+class Board
+  attr_accessor :next_turn, :moves_left
+  attr_reader :grid
+
+  def initialize
+    @grid = {
+      1 => :_, 2 => :_, 3 => :_,
+      4 => :_, 5 => :_, 6 => :_,
+      7 => :_, 8 => :_, 9 => :_
+    }
+    @moves_left = true
+    @next_turn = false
+  end
+
+  def display_grid
+    puts "  #{grid[1]} | #{grid[2]} | #{grid[3]} \n
+  #{grid[4]} | #{grid[5]} | #{grid[6]} \n
+  #{grid[7]} | #{grid[8]} | #{grid[9]}"
+  end
+
+  def make_move(player, position)
+    unless @grid[position] == :_
+      puts 'That position is already taken'
+      return false
+    end
+    @grid[position] = player.symbol
+    @next_turn = true
+  end
+
+  def winning_symbol
+    x = :x
+    o = :o
+
+    if grid[1] == x && grid[2] == x && grid[3] == x || # 123
+       grid[4] == x && grid[5] == x && grid[6] == x || # 456
+       grid[7] == x && grid[8] == x && grid[9] == x || # 789
+       grid[1] == x && grid[4] == x && grid[7] == x || # 147
+       grid[2] == x && grid[5] == x && grid[8] == x || # 258
+       grid[3] == x && grid[6] == x && grid[9] == x || # 369
+       grid[1] == x && grid[5] == x && grid[9] == x || # 159
+       grid[3] == x && grid[5] == x && grid[7] == x # 357
+
+      x
+
+    elsif grid[1] == o && grid[2] == o && grid[3] == o || # 123
+          grid[4] == o && grid[5] == o && grid[6] == o || # 456
+          grid[7] == o && grid[8] == o && grid[9] == o || # 789
+          grid[1] == o && grid[4] == o && grid[7] == o || # 147
+          grid[2] == o && grid[5] == o && grid[8] == o || # 258
+          grid[3] == o && grid[6] == o && grid[9] == o || # 369
+          grid[1] == o && grid[5] == o && grid[9] == o || # 159
+          grid[3] == o && grid[5] == o && grid[7] == o # 357
+
+      o
+    end
+  end
+
+  def check_moves_left
+    unless grid.value?(:_)
+      puts "No moves left!\nThe match has ended in a draw."
+      @moves_left = false
+    end
+  end
+end
