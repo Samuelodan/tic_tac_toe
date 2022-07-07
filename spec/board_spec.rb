72109f1680c0241adcc53dbe78c39bf8631d9493
Samuel ODaniels <samodan195@gmail.com>
2022-07-07 01:07:35 +0100

    Write tests for Board#make_move, one context

---
+++ spec/board_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+require_relative '../lib/players'
+
+RSpec.describe Board do
+  subject(:board) { described_class.new }
+  let(:xplayer) { instance_double(Players, symbol: :x) }
+
+  describe '#make_move' do
+    context 'when position is already taken' do
+      position = 3
+      before do
+        board.grid[3] = :y
+      end
+
+      it 'puts a message' do
+        expect(board).to receive(:puts).with('That position is already taken')
+        board.make_move(xplayer, position)
+      end
+
+      it 'does not register move' do
+        board.make_move(xplayer, position)
+        expect(board.grid[position]).to_not be :x
+      end
+    end
+  end
+end
