b458580fc01164150d68713d419d1f3c1038ea05
Samuel ODaniels <samodan195@gmail.com>
2022-07-08 02:54:26 +0100

    Write tests for Players#set_symbol

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../lib/players'
+
+RSpec.describe Players do
+  subject(:player) { Players.new }
+
+  describe '#set_symbol' do
+    context 'when argument is :x or :o' do
+      it 'sets symbol to argument' do
+        player.set_symbol(:x)
+        expect(player.symbol).to be :x
+      end
+      
+      it 'sets symbol to argument' do
+        player.set_symbol(:o)
+        expect(player.symbol).to be :o
+      end
+    end
+  end
+end
